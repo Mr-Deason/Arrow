6e0f04fcc89d10aab03eab1d3bd86cd2bf868fe0
yshen7 <yshen7@uw.edu>
2016-10-22 22:18:35 -0700

    fix

--- client/UDPClient.java
+++ client/UDPClient.java
@@ -5,38 +5,37 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
 
-
-
-
-
 public class UDPClient {
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String message = in.nextLine();
-		DatagramSocket socket = null;
-		try{
-		
+		try {
+
+			DatagramSocket socket = null;
 			socket = new DatagramSocket();
-			byte[] m = message.getBytes();
-			InetAddress hostname = InetAddress.getByName("127.0.0.1");
-			int port = 18409;
-			
-			//Send
-			DatagramPacket request = new DatagramPacket(m, message.length(), hostname, port);
-			socket.send(request);
-			
-			Thread.sleep(2000);
-			// Reply
-			byte[] buffer = new byte[1024];
-			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-			socket.receive(reply);
-			System.out.println("reply:" + new String(reply.getData()));
-			
-		}catch(Exception e) {
+			while (in.hasNext()) {
+				String message = in.nextLine();
+				byte[] m = message.getBytes();
+				InetAddress hostname = InetAddress.getByName("127.0.0.1");
+				int port = 18409;
+
+				// Send
+				DatagramPacket request = new DatagramPacket(m,
+						message.length(), hostname, port);
+				socket.send(request);
+
+				Thread.sleep(2000);
+				// Reply
+				byte[] buffer = new byte[1024];
+				DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+				socket.receive(reply);
+				System.out.println("reply:" + new String(reply.getData()).trim());
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
